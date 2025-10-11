178ef79ca842fd5442d9bc5b3d6e5377429fcb50
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-11 12:24:52 +0200

    feat: refactor next-sitemap configuration to use module.exports for app and marketing

--- apps/marketing/next-sitemap.config.js
+++ apps/marketing/next-sitemap.config.js
@@ -1,36 +1,33 @@
 /** @type {import('next-sitemap').IConfig} */
 const { SITE_URL } = process.env;
 
-export const siteUrl = SITE_URL || "http://localhost:3000";
-export const changefreq = "daily";
-export const priority = 0.7;
-export const generateRobotsTxt = true;
-export const exclude = [
-  "/server-sitemap-index.xml",
-  "/404",
-  "/admin/**",
-  "/api/**",
-];
-export const alternateRefs = [
-  {
-    href: SITE_URL || "http://localhost:3000",
-    hreflang: "en",
-  },
-  {
-    href: `${SITE_URL || "http://localhost:3000"}/fr`,
-    hreflang: "fr",
-  },
-  {
-    href: `${SITE_URL || "http://localhost:3000"}/pt`,
-    hreflang: "pt",
-  },
-];
-export const robotsTxtOptions = {
-  policies: [
+module.exports = {
+  siteUrl: SITE_URL || "http://localhost:3000",
+  changefreq: "daily",
+  priority: 0.7,
+  generateRobotsTxt: true,
+  exclude: ["/server-sitemap-index.xml", "/404", "/admin/**", "/api/**"],
+  alternateRefs: [
     {
-      userAgent: "*",
-      allow: "/",
+      href: SITE_URL || "http://localhost:3000",
+      hreflang: "en",
+    },
+    {
+      href: `${SITE_URL || "http://localhost:3000"}/fr`,
+      hreflang: "fr",
+    },
+    {
+      href: `${SITE_URL || "http://localhost:3000"}/pt`,
+      hreflang: "pt",
     },
   ],
-  additionalSitemaps: [`${SITE_URL}/server-sitemap-index.xml`],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: [`${SITE_URL}/server-sitemap-index.xml`],
+  },
 };

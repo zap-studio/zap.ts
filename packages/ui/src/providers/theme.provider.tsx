6a59d276b10e9992c4ae0d4fb6c7f709c55f8402
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-11 13:42:00 +0200

    feat: add @zap/ui package with theme provider and configuration

---
+++ packages/ui/src/providers/theme.provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+import "client-only";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type * as React from "react";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}

d8d984d24bac41134f92cf3fabe0817d64282562
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-12 12:35:13 +0200

    feat: implement utility functions for web push notifications

---
+++ packages/pwa/src/utils/index.ts
@@ -0,0 +1,52 @@
+import { PushNotificationError } from "@zap/errors";
+import type * as webpush from "web-push";
+
+const BASE64_PAD_LENGTH = 4;
+
+export function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat(
+    (BASE64_PAD_LENGTH - (base64String.length % BASE64_PAD_LENGTH)) %
+      BASE64_PAD_LENGTH
+  );
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
+
+export function arrayBufferToBase64(buffer: ArrayBuffer) {
+  let binary = "";
+
+  const bytes = new Uint8Array(buffer);
+
+  for (const byte of bytes) {
+    binary += String.fromCharCode(byte);
+  }
+
+  return window.btoa(binary);
+}
+
+export function subscriptionToWebPushSubscription(
+  subscription: PushSubscription
+): webpush.PushSubscription {
+  const p256dh = subscription.getKey("p256dh");
+  const auth = subscription.getKey("auth");
+
+  if (!(p256dh && auth)) {
+    throw new PushNotificationError("Invalid PushSubscription object");
+  }
+
+  return {
+    endpoint: subscription.endpoint,
+    keys: {
+      p256dh: arrayBufferToBase64(p256dh),
+      auth: arrayBufferToBase64(auth),
+    },
+  };
+}

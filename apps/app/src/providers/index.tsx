3880ea4b90b0f11dbdc72e51cabd2500def8cc29
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-12 21:54:41 +0200

    feat: refactor Providers component to integrate PostHog and Vercel providers with analytics configuration

--- apps/app/src/providers/index.tsx
+++ apps/app/src/providers/index.tsx
@@ -1,21 +1,35 @@
 "use client";
 import "client-only";
 
+import { PostHogProvider } from "@zap/analytics/posthog/providers";
+import { VercelProvider } from "@zap/analytics/vercel/providers";
 import { TanStackQueryProvider } from "@zap/async-state/tanstack-query/providers";
 import { BaseProviders } from "@zap/ui/providers/base";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { queryClient } from "@/lib/tanstack-query";
+import { analytics } from "@/lib/zap";
 
-type ProvidersProps = {
-  children: React.ReactNode;
-};
-
-export function Providers({ children }: ProvidersProps) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <BaseProviders>
       <NuqsAdapter>
         <TanStackQueryProvider queryClient={queryClient}>
-          {children}
+          <PostHogProvider
+            config={{
+              enablePostHog: analytics.POSTHOG.ENABLED,
+              apiKey: analytics.POSTHOG.API_KEY,
+              host: analytics.POSTHOG.HOST,
+            }}
+          >
+            {children}
+            <VercelProvider
+              config={{
+                enableVercelAnalytics: analytics.VERCEL.ANALYTICS.ENABLED,
+                enableVercelSpeedInsights:
+                  analytics.VERCEL.SPEED_INSIGHTS.ENABLED,
+              }}
+            />
+          </PostHogProvider>
         </TanStackQueryProvider>
       </NuqsAdapter>
     </BaseProviders>
--- apps/marketing/src/providers/index.tsx
+++ apps/marketing/src/providers/index.tsx
@@ -1,12 +1,29 @@
 "use client";
 import "client-only";
 
+import { PostHogProvider } from "@zap/analytics/posthog/providers";
+import { VercelProvider } from "@zap/analytics/vercel/providers";
 import { BaseProviders } from "@zap/ui/providers/base";
+import { analytics } from "@/lib/zap";
 
-type ProvidersProps = {
-  children: React.ReactNode;
-};
-
-export function Providers({ children }: ProvidersProps) {
-  return <BaseProviders>{children}</BaseProviders>;
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <BaseProviders>
+      <PostHogProvider
+        config={{
+          enablePostHog: analytics.POSTHOG.ENABLED,
+          apiKey: analytics.POSTHOG.API_KEY,
+          host: analytics.POSTHOG.HOST,
+        }}
+      >
+        {children}
+        <VercelProvider
+          config={{
+            enableVercelAnalytics: analytics.VERCEL.ANALYTICS.ENABLED,
+            enableVercelSpeedInsights: analytics.VERCEL.SPEED_INSIGHTS.ENABLED,
+          }}
+        />
+      </PostHogProvider>
+    </BaseProviders>
+  );
 }

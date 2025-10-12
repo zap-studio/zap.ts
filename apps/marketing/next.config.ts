e41f14c2e6be92c5e5e8368a304ee6eee873bf2e
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-12 13:09:00 +0200

    feat: reorganize imports in Next.js configuration files for consistency

--- apps/marketing/next.config.ts
+++ apps/marketing/next.config.ts
@@ -1,9 +1,6 @@
-import {
-  composeConfig,
-  createBaseConfig,
-  withBundleAnalyzer,
-  withMDX,
-} from "@zap/next-config";
+import { composeConfig, createBaseConfig } from "@zap/next-config";
+import { withBundleAnalyzer } from "@zap/next-config/plugins/bundle-analyzer";
+import { withMDX } from "@zap/next-config/plugins/mdx";
 
 const base = createBaseConfig("default");
 export default composeConfig(base, [withMDX, withBundleAnalyzer]);

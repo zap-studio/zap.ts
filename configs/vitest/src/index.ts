16421b71bfb2672f40592ec03022e89ed8a67c16
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-17 23:14:20 +0200

    feat: add example test files for Vitest and update shared configuration

--- configs/vitest/src/index.ts
+++ configs/vitest/src/index.ts
@@ -4,7 +4,7 @@ export const sharedConfig = defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
+    // setupFiles: ["./src/test/setup.ts"], // this file allows to set up the testing environment before each test run
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],

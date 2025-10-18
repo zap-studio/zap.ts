c1f7ac255bbc5b249c60328c2c5b619b4dd9911d
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-18 13:26:37 +0200

    fix: remove unnecessary blank line in db.dev.ts

--- packages/db/src/drizzle/db.dev.ts
+++ packages/db/src/drizzle/db.dev.ts
@@ -9,7 +9,6 @@ import { Pool } from "pg";
 import { type DatabaseSchema, schema } from "./schema";
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL_DEV || "" }); // TODO: to change with t3-env later
-
 export const db: NodePgDatabase<DatabaseSchema> = drizzlePg({
   client: pool,
   schema,

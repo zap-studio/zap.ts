51560a86c8fca69bd5a00db6e12baf145d340917
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-18 20:17:18 +0200

    feat: update package.json exports and add new constants and types files

--- packages/db/src/drizzle/types.ts
+++ packages/db/src/drizzle/types.ts
@@ -1,6 +1,6 @@
 import type { NeonHttpDatabase } from "drizzle-orm/neon-http";
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
-import type { DatabaseSchema } from "../schema";
+import type { DatabaseSchema } from "./schema";
 
 export type Database =
   | NodePgDatabase<DatabaseSchema>

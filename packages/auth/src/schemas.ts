16988dd1f493d83d2fa28bc8d2163ba0cd002c39
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-19 12:32:05 +0200

    feat: add validation schemas for login and registration forms

---
+++ packages/auth/src/schemas.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+import { AUTH_BASE_CONFIG } from ".";
+
+export const LoginFormSchema = z.object({
+  email: z.email(),
+  password: z.string().min(AUTH_BASE_CONFIG.MINIMUM_PASSWORD_LENGTH, {
+    message: `Password must be at least ${AUTH_BASE_CONFIG.MINIMUM_PASSWORD_LENGTH} characters.`,
+  }),
+});
+
+export const RegisterFormSchema = z
+  .object({
+    name: z
+      .string()
+      .min(AUTH_BASE_CONFIG.MINIMUM_USERNAME_LENGTH, {
+        message: `Name must be at least ${AUTH_BASE_CONFIG.MINIMUM_USERNAME_LENGTH} characters.`,
+      })
+      .max(AUTH_BASE_CONFIG.MAXIMUM_USERNAME_LENGTH, {
+        message: `Name must be at most ${AUTH_BASE_CONFIG.MAXIMUM_USERNAME_LENGTH} characters.`,
+      }),
+    email: z.email(),
+    password: z
+      .string()
+      .min(AUTH_BASE_CONFIG.MINIMUM_PASSWORD_LENGTH, {
+        message: `Password must be at least ${AUTH_BASE_CONFIG.MINIMUM_PASSWORD_LENGTH} characters.`,
+      })
+      .max(AUTH_BASE_CONFIG.MAXIMUM_PASSWORD_LENGTH, {
+        message: `Password must be at most ${AUTH_BASE_CONFIG.MAXIMUM_PASSWORD_LENGTH} characters.`,
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });

fb7edbf2be945e22e418bf29e766b0fe93035cf4
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-19 11:44:29 +0200

    fix: update import path for env module in next-sitemap.config.js files

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,5 +1,5 @@
 import { createNextSitemapConfig } from "@zap/seo/next-sitemap";
-import { env } from "./src/app/env";
+import { env } from "./src/app/env.ts";
 
 export default createNextSitemapConfig(env.SITE_URL, {
   // Override or add app-specific config here if needed

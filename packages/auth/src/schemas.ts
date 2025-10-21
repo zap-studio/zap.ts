f1b17b5a06dad2bf4203b7cdb11f821fd169a41b
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-21 08:47:03 +0200

    refactor(schemas): consolidate validation schemas for Login, Register, Reset, and Forgot Password forms

--- packages/auth/src/schemas.ts
+++ packages/auth/src/schemas.ts
@@ -1,64 +1,57 @@
 import z from "zod";
 import { AUTH_CONFIG } from ".";
 
+export const NameSchema = z
+  .string()
+  .min(
+    AUTH_CONFIG.FIELD_LENGTH.USERNAME.MIN,
+    `Name must be at least ${AUTH_CONFIG.FIELD_LENGTH.USERNAME.MIN} characters.`
+  )
+  .max(
+    AUTH_CONFIG.FIELD_LENGTH.USERNAME.MAX,
+    `Name must be at most ${AUTH_CONFIG.FIELD_LENGTH.USERNAME.MAX} characters.`
+  );
+
+export const MailSchema = z.email("Please enter a valid email address");
+
+export const PasswordSchema = z
+  .string()
+  .min(
+    AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN,
+    `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters.`
+  )
+  .max(
+    AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX,
+    `Password must be at most ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX} characters.`
+  );
+
 export const LoginFormSchema = z.object({
-  email: z.email(),
-  password: z.string().min(AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN, {
-    message: `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters.`,
-  }),
+  email: MailSchema,
+  password: PasswordSchema,
 });
 
-export const RegisterFormSchema = z.object({
-  name: z
-    .string()
-    .min(AUTH_CONFIG.FIELD_LENGTH.USERNAME.MIN, {
-      message: `Name must be at least ${AUTH_CONFIG.FIELD_LENGTH.USERNAME.MIN} characters.`,
-    })
-    .max(AUTH_CONFIG.FIELD_LENGTH.USERNAME.MAX, {
-      message: `Name must be at most ${AUTH_CONFIG.FIELD_LENGTH.USERNAME.MAX} characters.`,
-    }),
-  email: z.email(),
-  password: z
-    .string()
-    .min(AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN, {
-      message: `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters.`,
-    })
-    .max(AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX, {
-      message: `Password must be at most ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX} characters.`,
-    }),
-  confirm_password: z
-    .string()
-    .min(AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN, {
-      message: `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters.`,
-    })
-    .max(AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX, {
-      message: `Password must be at most ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX} characters.`,
-    }),
-});
+export const RegisterFormSchema = z
+  .object({
+    name: NameSchema,
+    email: MailSchema,
+    password: PasswordSchema,
+    confirm_password: PasswordSchema,
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
 
-export const ResetPasswordFormSchema = z.object({
-  password: z
-    .string()
-    .min(
-      AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN,
-      `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters`
-    )
-    .max(
-      AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX,
-      `Password must be at most ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX} characters`
-    ),
-  confirm_password: z
-    .string()
-    .min(
-      AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN,
-      `Password must be at least ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MIN} characters`
-    )
-    .max(
-      AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX,
-      `Password must be at most ${AUTH_CONFIG.FIELD_LENGTH.PASSWORD.MAX} characters`
-    ),
-});
+export const ResetPasswordFormSchema = z
+  .object({
+    password: PasswordSchema,
+    confirm_password: PasswordSchema,
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
 
 export const ForgotPasswordFormSchema = z.object({
-  email: z.email("Please enter a valid email address"),
+  email: MailSchema,
 });

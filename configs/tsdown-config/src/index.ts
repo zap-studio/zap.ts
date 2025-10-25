18b2652c71b4ae94d4d310601171e0a9ca1f3a6a
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-25 12:28:23 +0200

    chore: remove format option from tsdown configuration

--- configs/tsdown-config/src/index.ts
+++ configs/tsdown-config/src/index.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsdown";
 export function createConfig(overrides = {}) {
 	return defineConfig({
 		entry: "src/**/*.ts",
-		format: ["cjs", "esm"],
 		exports: true,
 		dts: true,
 		...overrides,

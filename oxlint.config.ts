257425ce0c7cffa92ffa21d17817e47a507e4aa2
alexandretrotel <trotelalexandre@proton.me>
2026-08-19 11:13:13 +0200

    chore: update zap-studio/oxlint package

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,12 +1,14 @@
-import base from "@zap-studio/oxlint/base";
-import jsxRuntimeAutomatic from "@zap-studio/oxlint/jsx-runtime-automatic";
-import react from "@zap-studio/oxlint/react";
-import reactA11y from "@zap-studio/oxlint/react-a11y";
-import reactDoctor from "@zap-studio/oxlint/react-doctor";
-import reactPerf from "@zap-studio/oxlint/react-perf";
-import stylex from "@zap-studio/oxlint/stylex";
-import tanstackRouter from "@zap-studio/oxlint/tanstack-router";
-import tanstackStart from "@zap-studio/oxlint/tanstack-start";
+import {
+  base,
+  jsxRuntimeAutomatic,
+  react,
+  reactA11y,
+  reactDoctor,
+  reactPerf,
+  stylex,
+  tanstackRouter,
+  tanstackStart,
+} from "@zap-studio/oxlint";
 import { defineConfig } from "oxlint";
 
 export default defineConfig({

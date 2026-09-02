1192b94f492fb3113971052cb7998ea0dad78f1b
alexandretrotel <trotelalexandre@proton.me>
2026-09-02 11:02:41 +0200

    fix: rework env variables needed

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -4,8 +4,14 @@ import { z } from "zod";
 
 export const env = createEnvironment({
   extends: [cloudflare],
-  server: { BETTER_AUTH_SECRET: z.string().min(32) },
-  client: {},
+  server: {
+    BETTER_AUTH_SECRET: z.string().min(32),
+    CLERK_SECRET_KEY: z.string().startsWith("sk_"),
+    CLERK_WEBHOOK_SIGNING_SECRET: z.string().startsWith("whsec_"),
+  },
+  client: {
+    VITE_CLERK_PUBLISHABLE_KEY: z.string().startsWith("pk_"),
+  },
   clientPrefix: "VITE_",
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,

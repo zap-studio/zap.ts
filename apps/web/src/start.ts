c313893c33bd0933214ee89b217817b9ee209ea6
alexandretrotel <trotelalexandre@proton.me>
2026-09-02 13:22:53 +0200

    feat: add clerk middleware

---
+++ apps/web/src/start.ts
@@ -0,0 +1,6 @@
+import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
+import { createStart } from "@tanstack/react-start";
+
+export const startInstance = createStart(() => ({
+  requestMiddleware: [clerkMiddleware()],
+}));

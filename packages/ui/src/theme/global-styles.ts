eb3ad30e7fe550f1ab52015d3cdd926cc0c4a31e
alexandretrotel <trotelalexandre@proton.me>
2026-09-03 17:31:50 +0200

    fix: rework global style

--- packages/ui/src/theme/global-styles.ts
+++ packages/ui/src/theme/global-styles.ts
@@ -5,10 +5,16 @@ import { fontFamily } from "../tokens/font-family.stylex";
 
 const styles = stylex.create({
   body: {
+    MozOsxFontSmoothing: "grayscale",
+    WebkitFontSmoothing: "antialiased",
+    WebkitTapHighlightColor: "transparent",
     margin: 0,
     backgroundColor: colors.bg,
     color: colors.fg,
     fontFamily: fontFamily.sans,
+    scrollbarColor: `${colors.mutedFg} transparent`,
+    scrollbarWidth: "thin",
+    textSizeAdjust: "100%",
     transitionDuration: "150ms",
     transitionProperty: "background-color, color",
     transitionTimingFunction: "ease-out",

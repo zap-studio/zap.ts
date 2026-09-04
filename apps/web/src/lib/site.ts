9dafb670fdcf3faefeaf3c57ad9225507f09c796
alexandretrotel <trotelalexandre@proton.me>
2026-09-04 16:04:08 +0200

    feat: add more informations

--- apps/web/src/lib/site.ts
+++ apps/web/src/lib/site.ts
@@ -1,3 +1,9 @@
-export const SITE_URL = "https://zap-ts.zapstudio.dev";
+// TODO: Replace with your informations.
+
+const DEFAULT_SITE_URL = "https://zap-ts.zapstudio.dev";
+
+export const SITE_URL = import.meta.env["VITE_SITE_URL"] ?? DEFAULT_SITE_URL;
 export const SITE_NAME = "zap.ts";
+export const SITE_TAGLINE = "Ship your SaaS";
 export const SITE_DESCRIPTION = "A SaaS starter kit.";
+export const SITE_TWITTER_HANDLE = "@zapstudio";
